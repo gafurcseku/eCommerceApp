8188f1b82f5841c8e78967137fad391b5f3d909f
Abdul Gafur <abdul.gafur@rootnext.com>
2023-08-10 15:37:45 +0600

    Initial Commit

---
+++ CommerceApp/CommerceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommerceAppApp.swift
+//  CommerceApp
+//
+//  Created by RootNext15 on 10/8/23.
+//
+
+import SwiftUI
+
+@main
+struct CommerceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
